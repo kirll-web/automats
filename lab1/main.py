5a16b0ce43abfe511c0c641e0dcad82c6d96481a
Reghan <reghansan@gmail.com>
2024-11-17 12:37:51 +0300

    [lab-1] fix

--- lab1/main.py
+++ lab1/main.py
@@ -12,11 +12,11 @@ COMMAND_MOORE_TO_MEALY = "moore-to-mealy"
 def main(args):
     #command = args[0]
     #command = COMMAND_MEALY_TO_MOORE  # args[0]
-    command = COMMAND_MOORE_TO_MEALY  # args[0]
-    # input_file_name = args[1]
-    # output_file_name = args[2]
-    input_file_name = "6moo.csv" #moore to mealy
-    output_file_name = "6mea.csv"  #moore to mealy
+    command = args[0]
+    input_file_name = args[1]
+    output_file_name = args[2]
+    #input_file_name = "6moo.csv" #moore to mealy
+    #output_file_name = "6mea.csv"  #moore to mealy
     #input_file_name = "4mea.csv"  # mealy to moore
     #output_file_name = "4moo.csv"  # mealy to moore
     input_file = open(input_file_name, "r")
