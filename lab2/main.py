2cc8fdb8f116271cec2b17e9c4f20bbf83033d2f
Reghan <reghansan@gmail.com>
2024-11-24 00:49:22 +0300

    [lab2] main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,6 +1,7 @@
 import sys
 
 from mealy import mealy_transform_to_min
+from moore import moore_transform_to_min
 
 COMMAND_MEALY = "mealy"
 COMMAND_MOORE = "moore"
@@ -9,12 +10,12 @@ COMMAND_MOORE = "moore"
 # program moore-to-mealy moore.csv mealy.csv
 # program mealy-to-moore mealy.csv moore.csv
 def main(args):
-    command = COMMAND_MEALY #args[0]
-    input_file_name = "1mea.csv" #args[1]
-    output_file_name = "mealy_min.csv" #args[2]
-    input_file = open(input_file_name, "r")
-    output_file = open(output_file_name, "w")
-    mealy_transform_to_min(input_file, output_file)
+    input_file = open(args[1], "r")
+    output_file = open(args[2], "w+")
+    if args[0] == COMMAND_MEALY:
+        mealy_transform_to_min(input_file, output_file)
+    if args[0] == COMMAND_MOORE:
+        moore_transform_to_min(input_file, output_file)
 
 
 if __name__ == '__main__':
