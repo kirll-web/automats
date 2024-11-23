1363a3a09408edbbcb4a03938d3556b5b6394c36
Reghan <reghansan@gmail.com>
2024-11-24 02:55:32 +0300

    [lab2] add moore

---
+++ lab2/moore.py
@@ -0,0 +1,109 @@
+NAME_POINTS = "Q"
+SEPARATOR = ";"
+NAME_CLASS = "CLASS"
+NAME_UNDERCLASS = "UNDERCLASS"
+TAG_CLASS = "C"
+
+class Ptr:
+    def __init__(self, value):
+        self.value: str = value
+        self.next = dict()
+        self.prev = dict()
+
+
+def moore_transform_to_min(input_file, output_file):
+    lines = input_file.readlines()
+    mealy_mass = get_mealy_mass(lines)
+    mealy_mass = remove_unreacheble_state(mealy_mass)
+    map_classes = dict()
+
+    for index, ch_line in enumerate(mealy_mass):
+        if ch_line == NAME_POINTS: continue
+        column = mealy_mass[ch_line]
+
+
+
+
+    print_mealy(mealy_mass, output_file)
+
+
+def the_first_division_into_classes(mealy_mass):
+    for index, line in enumerate(mealy_mass):
+        if line == NAME_POINTS: continue
+        #for index2, line in enumerate(mealy_mass):
+
+
+def remove_unreacheble_state(mealy_mass):
+    graph = dict()
+
+    for i, ch in enumerate(mealy_mass[NAME_POINTS]):
+        graph[ch] = Ptr(ch)
+
+    for i, new_point in enumerate(mealy_mass[NAME_POINTS]):
+        for k, ch in enumerate(mealy_mass):
+            if k in range(0, 1):
+                continue
+            else:
+                s = mealy_mass[ch][i]
+                if s[0] not in graph: graph[s] = Ptr(s[0])
+                graph[new_point].next[s[0]] = graph[s[0]]
+                graph[s[0]].prev[new_point] = graph[new_point]
+
+    has_unreach = True
+    while has_unreach:
+        has_unreach = False
+        new_graph = dict()
+        for i, ptr in enumerate(graph):
+            if ptr != mealy_mass[NAME_POINTS][0] and len(graph[ptr].prev) == 0:
+                has_unreach = True
+                for b, nextPtr in enumerate(graph[ptr].next):
+                    del graph[nextPtr].prev[ptr]
+            else:
+                new_graph[ptr] = graph[ptr]
+        graph = new_graph
+
+    new_mealy_mass = dict()
+    new_mealy_mass[NAME_POINTS] = []
+    for i, ch in enumerate(mealy_mass[NAME_POINTS]):
+        if ch not in graph: continue
+        for k, line in enumerate(mealy_mass):
+            if line not in new_mealy_mass: new_mealy_mass[line] = list()
+            new_mealy_mass[line].append(mealy_mass[line][i])
+
+    return  new_mealy_mass
+
+
+def get_mealy_mass(lines):
+    mass = dict()
+    mass[NAME_POINTS] = []
+    temp = lines[0].strip().split(SEPARATOR)
+    for i, item in enumerate(temp):
+        if item != "": mass[NAME_POINTS].append(item)
+
+    for i, line in enumerate(lines):
+        if i == 0: continue
+        temp = line.strip().split(SEPARATOR)
+        for k, t in enumerate(temp):
+            if i > 0 and k > 0:
+                temp[k] = t.strip().split("/")
+            else: temp[k] = t
+        mass[temp[0]] = []
+        mass[temp[0]] = temp[1:]
+    return mass
+
+def print_mealy(mealy_mass, output_file):
+    for i, line in enumerate(mealy_mass):
+        if line == NAME_POINTS:
+            output_file.write(";")
+        else:
+            output_file.write(f"{line};")
+        for k, ch in enumerate(mealy_mass[line]):
+            if line == NAME_POINTS:
+                output_file.write(ch)
+                if k < len(mealy_mass[line]) - 1:
+                    output_file.write(SEPARATOR)
+                continue
+            output_file.write(f"${ch[0]}/${ch[1]}")
+            if k < len(mealy_mass[line]) - 1:
+                output_file.write(SEPARATOR)
+        output_file.write("\n")
