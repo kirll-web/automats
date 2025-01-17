83549a65c792289d75b15a00f461fdc66c2e2e53
Reghan <reghansan@gmail.com>
2025-01-18 01:59:55 +0300

    [lab6] +- рабочая версия

--- lab6/main.py
+++ lab6/main.py
@@ -5,9 +5,10 @@ import sys
 TOKEN_PATTERNS = [
     ("WHITESPACE", r"\s"),
     ("LINE_COMMENT", r"//.*"),
+    ("BLOCK_COMMENT", r"\{(?:.|\n)*?\}"),  # Многострочные комментарии в одной строке
     ("START_BLOCK_COMMENT", r"\{\s*.*"),  # Многострочные комментарии
     ("END_BLOCK_COMMENT", r"(?:.|\n)*?\}"),  # Многострочные комментарии
-    ("BLOCK_COMMENT", r"\{(?:.|\n)*?\}"),  # Многострочные комментарии в одной строке
+
     ("ARRAY", r"(?i)\bARRAY\b"),
     ("BEGIN", r"(?i)\bBEGIN\b"),
     ("ELSE", r"(?i)\bELSE\b"),
@@ -22,9 +23,9 @@ TOKEN_PATTERNS = [
     ("VAR", r"(?i)\bVAR\b"),
     ("IDENTIFIER", r"[a-zA-Z_][a-zA-Z0-9_]*"),
     ("STRING", r"'(?:[^'\\]|\\.)*'"),
-    ("FLOAT", r"\b\d+\.\d+\b"),
     ("INTEGER", r"\b\d+\b"),
-    ("MULTIPLICATION", r"\*"),
+    ("FLOAT", r"^\d+(\.\d+)?([eE][+-]?\d+)?$"),  # Числа с плавающей точкой или экспонентой
+    ("INTEGER", r"^(?<![\d.])\b\d+\b(?![\d.])$"),
     ("PLUS", r"\+"),
     ("MINUS", r"-"),
     ("DIVIDE", r"/"),
@@ -43,67 +44,82 @@ TOKEN_PATTERNS = [
     ("COLON", r":"),
     ("ASSIGN", r":="),
     ("DOT", r"\."),
-
 ]
 
+class Token:
+    def __init__(self, name, value, line_number, start_position):
+        self.name = name
+        self.value = value
+        self.line_number = line_number
+        self.start_position = start_position
+
 class PascalLexer:
-    def __init__(self, input_file):
-        self.file = open(input_file, 'r', encoding='utf-8')
+    def __init__(self, input_file_name):
+        self.file = open(input_file_name, 'r', encoding='utf-8')
         self.line_number = 0
         self.current_line = ''
+        self.current_words = []
+        self.position_word = 0
         self.position = 0
 
     def next_line(self):
-        self.current_line = self.file.readline()
-        if self.current_line:
+        self.current_line = next(self.file, None)  # Возвращает None на конце файла
+
+        if not self.current_line  is None:
+            self.current_line = self.current_line.strip()
             self.line_number += 1
             self.position = 0
+            self.current_words = self.current_line.split(" ")
+            self.position_word = 0
+
             return True
         return False
 
     def next_token(self):
         while True:
-            if self.position >= len(self.current_line):
+            if self.position_word >= len(self.current_words):
                 if not self.next_line():
                     return None
 
-            text = self.current_line[self.position:]
+            text = self.current_words[self.position_word]
             i = 0
             while i < len(TOKEN_PATTERNS):
                 token_type, pattern = TOKEN_PATTERNS[i]
                 regex = re.compile(pattern)
-                match = regex.match(text)
+                match = regex.fullmatch(text)
                 if match:
                     value = match.group(0)
                     start_position = self.position + 1
                     self.position += len(value)
+                    self.position_word += 1
 
-                    if token_type in ["WHITESPACE"]:
-                        text = text[1:]
-                        i = 0
-                        continue
                     if token_type in ["START_BLOCK_COMMENT"]:
                         while True:
-                            if not self.next_line():
-                                return f"BAD: ({self.line_number},{start_position}) \"{value}\""
+                            if self.position_word >= len(self.current_words) and not self.next_line():
+                                return Token("BAD", value, self.line_number, start_position)
                             regex = re.compile(r"(?:.|\n)*?\}")
-                            match = regex.match(self.current_line)
+                            match = regex.match(self.current_words[self.position_word])
                             if match:
-                                value += match.group(0)
-                                self.position += len(match.group(0))
-                                return f"BLOCK_COMMENT ({self.line_number},{start_position}) \"{value}\""
+                                value += f"\n{match.group(0)}"
+                                self.position_word += 1
+                                return Token("BLOCK_COMMENT", value, self.line_number, start_position)
                             else:
-                                value += self.current_line
+                                value += f"\n{self.current_words[self.position_word]}"
+                                self.position_word += 1
+                    if token_type in ["LINE_COMMENT"]:
+                        value += " ".join(self.current_words[self.position_word:])
+                        self.next_line()
+                        return Token(token_type, value, self.line_number, start_position)
+
                     if len(value) > 256:
-                        return f"BAD: ({self.line_number},{start_position}) \"{value}\""
-                    return f"{token_type} ({self.line_number},{start_position}) \"{value}\""
+                        return Token("BAD", value, self.line_number, start_position)
+                    return Token(token_type, value, self.line_number, start_position)
                 i += 1
 
-            if self.position < len(self.current_line):
-                bad_char = self.current_line[self.position]
-                start_position = self.position + 1
-                self.position += 1
-                return f"BAD: ({self.line_number},{start_position}) \"{bad_char}\""
+            bad_char = self.current_words[self.position_word]
+            start_position = self.position + len(bad_char)
+            self.position_word += 1
+            return Token("BAD", bad_char, self.line_number, start_position)
 
     def close(self):
         self.file.close()
@@ -122,8 +138,11 @@ if __name__ == "__main__":
             token = lexer.next_token()
             if token is None:
                 break
-            print(token)
-            output.write(token + '\n')
+            if token.name == "LINE_COMMENT" or token.name == "BLOCK_COMMENT":
+                continue
+
+            print(f"{token.name} ({token.line_number}, {token.start_position}) \"{token.value}\"")
+            output.write(f"{token.name} ({token.line_number}, {token.start_position}) \"{token.value}\"")
 
     lexer.close()
 
